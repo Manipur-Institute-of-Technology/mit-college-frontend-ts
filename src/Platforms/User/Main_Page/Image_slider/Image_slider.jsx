c62d945d7d5f32e273af13f95cea8d3cc6594623
ArickYumnam <bungoyumnam@7gmail.com>
2024-11-07 17:54:58 +0530

    Tested

--- src/Platforms/User/Main_Page/Image_slider/Image_slider.jsx
+++ src/Platforms/User/Main_Page/Image_slider/Image_slider.jsx
@@ -14,34 +14,34 @@ export function Image_slider() {
       className="crsl"
     >
       <div className="items">
-        <img className="img_1" src="./images/Main_page/img_1.jpg" alt=""></img>
+        <img className="img_1" src="./Images/Main_page/img_1.jpg" alt=""></img>
         <p className="legend">
           MIT WEEK 2018 OPENING CEREMONY (12th NOV, 2018)
         </p>
       </div>
       <div className="items">
-        <img className="img_1" src="./images/Main_page/img_2.jpg" alt=""></img>
+        <img className="img_1" src="./Images/Main_page/img_2.jpg" alt=""></img>
         <p className="legend">
           DIGNITARIES AT THE VALEDICTORY FUNCTION OF MIT WEEK 2018 ON 17th NOV,
           2018
         </p>
       </div>
       <div className="items">
-        <img className="img_1" src="./images/Main_page/img_3.jpg" alt=""></img>
+        <img className="img_1" src="./Images/Main_page/img_3.jpg" alt=""></img>
         <p className="legend">
           11TH GB Meeting MIT. With Prof. A.P. Pandey, Hon'ble V.C. M.U. Prof.
           A.K. Saxena, Mentor (AICTE-NEQIP) and other Members.
         </p>
       </div>
       <div className="items">
-        <img className="img_1" src="./images/Main_page/img_4.jpg" alt=""></img>
+        <img className="img_1" src="./Images/Main_page/img_4.jpg" alt=""></img>
         <p className="legend">
           SPEECH FROM SHRI JARNAIL SINGH, ADMINISTRATOR, MANIPUR UNIVERSITY AT
           THE VALEDICTORY FUNCTION OF MIT WEEK 2018
         </p>
       </div>
       <div className="items">
-        <img className="img_1" src="./images/Main_page/img_5.jpg" alt=""></img>
+        <img className="img_1" src="./Images/Main_page/img_5.jpg" alt=""></img>
         <p className="legend">
           Inauguration of AICTE sponsored 3-days Faculty Development Programme
           on “Universal Human Values
