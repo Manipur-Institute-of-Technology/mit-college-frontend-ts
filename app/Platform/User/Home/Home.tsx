03c791e58f80acaf149fedc924c1b57ecb9f88ff
Venus Waikhom <apocalyse7@gmail.com>
2025-03-13 14:08:47 +0530

    Updated File managment and WebApp icon

---
+++ app/Platform/User/Home/Home.tsx
@@ -0,0 +1,10 @@
+import ImageCarousel from "./ImageCarousel/ImageCarrousel";
+
+export default function Home() {
+  return (
+    <div className="relative">
+      <ImageCarousel />
+      <h1>This is Home</h1>
+    </div>
+  );
+}
--- app/layouts/PublicContentLayout.tsx
+++ app/layouts/PublicContentLayout.tsx
@@ -1,35 +1,36 @@
 import { Outlet, useLocation } from "react-router";
 import { navigationData as navigation } from "../mock/navbar";
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/PublicFooter";
-import ImageCarousel from "../components/ImageCarousel/ImageCarrousel";
+import Navbar from "../Common/Navbar/Navbar";
+import Footer from "../Common/Footer/PublicFooter";
+import ImageCarousel from "../Platform/User/Home/ImageCarousel/ImageCarrousel";
 import type { Route } from "./+types/PublicContentLayout";
 
 export default function PublicLayout() {
-	const location = useLocation();
+  const location = useLocation();
 
-	return (
-		<>
-			<Navbar navigation={navigation} />
-			<main
-				style={{
-					backgroundImage: `radial-gradient(circle at center, rgba(200, 200, 200, 0.8) 0 1px, transparent 1px 18px), 
+  return (
+    <>
+      <Navbar navigation={navigation} />
+      <main
+        style={{
+          backgroundImage: `radial-gradient(circle at center, rgba(200, 200, 200, 0.8) 0 1px, transparent 1px 18px), 
 									radial-gradient(circle at center, transparent 30%, rgba(200, 200, 200, 0.8))`,
-					backgroundRepeat: "repeat",
-					backgroundSize: "18px 18px, 100vw 100vh",
-					backgroundAttachment: "fixed",
-				}}>
-				{location.pathname === "/" && (
-					<div className="mt-8 mb-2">
-						<ImageCarousel />
-					</div>
-				)}
+          backgroundRepeat: "repeat",
+          backgroundSize: "18px 18px, 100vw 100vh",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        {/* {location.pathname === "/" && (
+          <div className="mt-8 mb-2">
+            <ImageCarousel />
+          </div>
+        )} */}
 
-				<div className="mx-auto max-w-7xl px-0 py-6 sm:px-6 lg:px-0 border border-black min-h-[100vh]">
-					<Outlet />
-				</div>
-			</main>
-			<Footer />
-		</>
-	);
+        <div className="mx-auto max-w-7xl px-0 py-6 sm:px-6 lg:px-0 border border-black min-h-[100vh]">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
