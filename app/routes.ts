5c443e57b4f14982062c86e1923ca6c59a212eaa
Lenin Khangjrakpam <leninkhangjrakpam@gmail.com>
2025-04-09 00:08:47 +0530

    feat: add user defined custom route to appp route

--- app/routes.ts
+++ app/routes.ts
@@ -1,21 +1,81 @@
 import {
 	type RouteConfig,
+	type RouteConfigEntry,
 	index,
 	layout,
 	prefix,
 	route,
 } from "@react-router/dev/routes";
 
-export default [
+import dotenv from "dotenv";
+import type { CustomRoute } from "./types/api/customRoute.type";
+const config = dotenv.config({ path: "./app.config.env" });
+
+const routes = async (
+	predefinedRoutes: RouteConfigEntry[],
+): Promise<RouteConfig> => {
+	const routeUrl = config?.parsed?.ROUTE_API_URL!;
+	try {
+		// Fetch user defined routes from API
+		const res = await fetch(routeUrl);
+		const customRoutes = (await res.json()) as CustomRoute[];
+		console.info("All good!, route configured with user defined routes");
+
+		return [
+			...predefinedRoutes,
+			// Custom defined routes
+			layout(
+				"./layouts/PublicContentLayout.tsx",
+				{ id: "publicContentLayoutForCustomRoutes" },
+				[
+					...customRoutes.map((d) =>
+						route(d.route, "./routes/page.tsx", { id: d.route }),
+					),
+				],
+			),
+		] satisfies RouteConfig;
+	} catch (err) {
+		console.error(
+			`Error fetching user defined routes from API\nCheck API route ${routeUrl} is working!`,
+		);
+		console.error("Err: ", err);
+		console.warn("configured routes without user defined routes");
+
+		// No Custom defined routes
+		return predefinedRoutes satisfies RouteConfig;
+	}
+};
+
+export const predefinedRoutes = [
 	// public routes
 	// route("/test", "./routes/test.tsx"),
-	// route("/test1", "./routes/test1.tsx"),
+	route("/test1", "./routes/test1.tsx"),
 	layout("./layouts/PublicContentLayout.tsx", [
 		index("routes/home.tsx"),
 		route("/home", "./routes/home.tsx", { id: "home1" }),
 		route("/about", "./routes/about.tsx"),
 		route("/contact", "./routes/contact.tsx"),
 		route("/editor", "./routes/editor.tsx"),
+		// faculty routes
+		...prefix("/faculty", [
+			index("./routes/faculty/index.tsx"), // Faculty Search Routes
+			...prefix(":facultyId", [
+				// Faculty Profile Routes
+				layout("./layouts/FacultyLayout.tsx", [
+					index("./routes/faculty/[facultyId]/about.tsx"),
+					route("aboutme", "./routes/faculty/[facultyId]/about.tsx", {
+						id: "facultyAbout1",
+					}),
+					route("teaching", "./routes/faculty/[facultyId]/teaching.tsx"),
+					route("research", "./routes/faculty/[facultyId]/research.tsx"),
+					route(
+						"publications",
+						"./routes/faculty/[facultyId]/publications.tsx",
+					),
+					route("students", "./routes/faculty/[facultyId]/students.tsx"),
+				]),
+			]),
+		]),
 	]),
 	// CMS Route
 	...prefix("cms", [
@@ -50,3 +110,5 @@ export default [
 	// 	[route("*", "routes/notFound.tsx")],
 	// ),
 ] satisfies RouteConfig;
+
+export default routes(predefinedRoutes) satisfies Promise<RouteConfig>;
