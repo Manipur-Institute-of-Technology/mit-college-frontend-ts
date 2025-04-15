afb81b70d72393bbf2e1c467f2ba57cb03a120fd
Lenin Khangjrakpam <leninkhangjrakpam@gmail.com>
2025-04-15 22:11:07 +0530

    feat: mod faculty/publication page with new components

--- app/pages/faculty/[facultyId]/publications/PublicationChart.tsx
+++ app/pages/faculty/[facultyId]/publications/PublicationChart.tsx
@@ -1,8 +1,19 @@
 import moment from "moment";
-import React from "react";
 import type { FacultyDetailProfile } from "~/types/api/faculty.type";
+import React, { useEffect, useMemo } from "react";
+import { Fullscreen, Loader2 } from "lucide-react";
+import { useModal } from "~/hooks/useModal";
+
+import BarChart from "~/components/BarChart";
+import ModalBody from "./LargePublicationChart";
 
 const PublicationChart: React.FC<FacultyDetailProfile> = (props) => {
+	const { setIsOpen, ModalComponent } = useModal();
+
+	useEffect(() => {
+		setIsOpen(true);
+	}, []);
+
 	const data = React.useMemo(() => {
 		const publications = props.researchDetail.publications;
 		const countByYear = publications.reduce(
@@ -19,37 +30,66 @@ const PublicationChart: React.FC<FacultyDetailProfile> = (props) => {
 			.map(([year, count]) => ({ year, count }));
 	}, [props.researchDetail.publications]);
 
+	const width = 290,
+		height = 200;
+	const barProps: React.ComponentProps<typeof BarChart> = useMemo(
+		() => ({
+			y: Array.from({ length: 8 }, () => Math.ceil(Math.random() * 30)),
+			x: Array.from({ length: 8 }, (_, i) => String(2000 + i)),
+			xLabel: "Years",
+			yLabel: "Number of publication",
+			width,
+			height,
+		}),
+		[width, height],
+	);
+
 	return (
-		<div className="p-2 rounded-md bg-slate-200 shadow-md font-roboto">
-			<div className="relative w-fit text-wrap font-semibold">
-				Publication Statistics
+		<div className="p-2 rounded-md bg-slate-100 shadow-md font-roboto w-full border border-slate-300">
+			<div className="inline-flex items-center justify-between w-full px-2 py-1">
+				<div className="relative w-fit text-wrap text-md font-semibold font-roboto text-slate-600">
+					Publication Statistics
+				</div>
+				<button
+					className="border border-slate-400 rounded-md p-0.5 hover:bg-slate-100 hover:cursor-pointer"
+					onClick={() => setIsOpen(true)}>
+					<Fullscreen size={18} />
+				</button>
 			</div>
 
-			<div className="w-full overflow-x-auto">
-				<div className="min-w-[600px] h-[300px] p-4">
-					<div className="relative h-full flex items-end gap-2">
-						{data.map(({ year, count }, i) => (
-							<div
-								key={i}
-								className="group flex-1 flex flex-col items-center border"
-								style={{ height: "100%" }}>
-								<div
-									className="w-2 bg-blue-500 hover:bg-blue-600 transition-all duration-200 rounded-t-md relative border"
-									style={{
-										height: `${(count / Math.max(...data.map((d) => d.count))) * 80}%`,
-									}}>
-									<div className="absolute -top-8 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 bg-slate-800 text-white px-2 py-1 rounded text-sm transition-opacity">
-										{count} papers
-									</div>
-								</div>
-								<div className="mt-2 text-sm text-slate-600">
-									{year}
-								</div>
-							</div>
-						))}
+			{/* TODO: Dynamic import of d3 chart */}
+			<div
+				className="relative overflow-x-auto my-2 max-w-full"
+				style={{
+					scrollbarWidth: "thin",
+					scrollbarColor: "#cbd5e1 #f1f5f9",
+				}}>
+				<svg
+					className="bg-slate-100 rounded-md mx-auto"
+					width={width}
+					height={height}>
+					<BarChart {...barProps} />
+				</svg>
+			</div>
+
+			<div
+				className="relative overflow-x-auto my-2"
+				style={{
+					scrollbarWidth: "thin",
+					scrollbarColor: "#cbd5e1 #f1f5f9",
+				}}>
+				<div className="bg-slate-50 rounded-md w-full h-[200px] flex justify-center items-center flex-col">
+					<Loader2 size={18} className="animate-spin origin-center" />
+					<div className="font-roboto text-sm text-slate-600 text-center">
+						Crunching, the latest data for you. <br />
+						Hang, tight ...
 					</div>
 				</div>
 			</div>
+
+			<ModalComponent>
+				<ModalBody setIsOpen={setIsOpen} />
+			</ModalComponent>
 		</div>
 	);
 };
--- app/pages/faculty/[facultyId]/publications/PublicationListContainer.tsx
+++ app/pages/faculty/[facultyId]/publications/PublicationListContainer.tsx
@@ -1,10 +1,12 @@
-import { Search } from "lucide-react";
-import { useState } from "react";
+import { ChevronDown, ChevronUp, Search, Tag, X } from "lucide-react";
+import React, { useState } from "react";
 import InputGrp from "~/components/InputGrp";
 import type { FacultyDetailProfile } from "~/types/api/faculty.type";
 import Pagination from "./Pagination";
 import DropDownMenu from "~/components/DropDownMenu";
 import PublicationLists from "./PublicationLists";
+import DropDownMenus from "~/components/DropDownMenus";
+import { FilterInput, type FilterSchema } from "./Filter";
 
 const PublicationListContainer: React.FC<FacultyDetailProfile> = (props) => {
 	const [selectedPaginationIndx, setSelectedPaginationIndx] = useState(0);
@@ -17,37 +19,148 @@ const PublicationListContainer: React.FC<FacultyDetailProfile> = (props) => {
 		"date (Oldest First)",
 	].map((d) => ({ name: d, value: d.toLowerCase() }));
 
+	const [advFilterActive, setAdvFilterActive] = useState(false);
+	const [searchFilter, setSearchFilter] = useState<FilterSchema>({
+		fromYear: undefined,
+		endYear: undefined,
+		pubTypes: [],
+		keywords: undefined,
+	});
+
+	const containValidFilter = (filter: FilterSchema): boolean => {
+		return filter.fromYear ||
+			filter.endYear ||
+			filter.pubTypes.length > 0 ||
+			(filter.keywords && filter.keywords.length > 0)
+			? true
+			: false;
+	};
+
 	return (
 		<div className="relative w-full font-roboto">
-			<div className="rounded-b-md shadow-md bg-slate-50 mt-4 mx-1 sm:mx-2 xl:mx-0 relative border-t-6 border-t-slate-500 p-4">
+			<div className="rounded-b-md shadow-md bg-slate-50 mx-1 sm:mx-2 xl:mx-0 relative border-t-6 border-t-slate-500 p-4">
 				<h1 className="uppercase font-semibold text-lg">
 					Publications
 				</h1>
 				<div>
-					<InputGrp className="w-full my-2">
-						<>
-							<Search size={18} className="w-fit" />
-							<input
-								type="text"
-								className="px-2 py-1 outline-none w-full"
-								placeholder="Search by title"
-							/>
-						</>
-					</InputGrp>
-					<Pagination firstPage={1} lastPage={10} currPage={5} />
+					<div className="flex md:flex-nowrap flex-wrap items-center gap-1 my-2">
+						<InputGrp className="w-full my-0.5">
+							<>
+								<Search size={18} className="w-fit" />
+								<input
+									type="text"
+									className="px-2 py-1 outline-none w-full"
+									placeholder="Search by title"
+								/>
+							</>
+						</InputGrp>
+						<button className="hover:cursor-pointer rounded-md bg-slate-200 w-full md:w-fit px-2 py-1 hover:bg-slate-300 hover:shadow-md">
+							Search
+						</button>
+					</div>
+
+					{containValidFilter(searchFilter) && (
+						<div className="text-sm font-roboto my-2 p-2">
+							<div className="text-md">Filters:</div>
+							<div className="text-sm ">
+								<div className="flex flex-row flex-wrap gap-2">
+									{searchFilter.fromYear && (
+										<div className="px-2 py-0.5 border border-slate-400 bg-slate-200 rounded-full">
+											From: {searchFilter.fromYear}
+										</div>
+									)}
+									{searchFilter.endYear && (
+										<div className="px-2 py-0.5 border border-slate-400 bg-slate-200 rounded-full">
+											To: {searchFilter.endYear}
+										</div>
+									)}
+								</div>
+								{searchFilter.pubTypes.length > 0 && (
+									<div className="inline-flex items-start gap-2 my-1">
+										<span className="py-1">
+											Publication Type:
+										</span>
+										<div className="flex flex-row flex-wrap gap-x-2 gap-y-1">
+											{searchFilter.pubTypes.map(
+												(d, i) => (
+													<div
+														key={i}
+														className="px-2 py-0.5 capitalize border border-slate-400 bg-slate-200 rounded-full">
+														{d}
+													</div>
+												),
+											)}
+										</div>
+									</div>
+								)}
+								{searchFilter.keywords &&
+									searchFilter.keywords.length > 0 && (
+										<div className="inline-flex items-start gap-2 my-1">
+											<span className="py-1">
+												Keywords:
+											</span>
+											<div className="flex flex-row flex-wrap gap-x-2 gap-y-1">
+												{searchFilter.keywords
+													.split(",")
+													.map((d, i) => (
+														<div
+															key={i}
+															className="capitalize py-0.5 px-2 border border-slate-400 bg-slate-200 rounded-full">
+															{d}
+														</div>
+													))}
+												{Array.from(
+													{ length: 12 },
+													(_, i) => (
+														<div
+															key={i}
+															className="capitalize py-0.5 px-2 border border-slate-400 bg-slate-200 rounded-full">
+															Neuroscience
+														</div>
+													),
+												)}
+											</div>
+										</div>
+									)}
+							</div>
+						</div>
+					)}
+
+					<button
+						className="bg-slate-200 text-sm font-roboto p-2 rounded-md inline-flex items-center gap-x-2"
+						onClick={() => setAdvFilterActive((s) => !s)}>
+						<span>Filter By</span>
+						{advFilterActive ? (
+							<ChevronUp size={18} />
+						) : (
+							<ChevronDown size={18} />
+						)}
+					</button>
+
+					{advFilterActive && (
+						<FilterInput setFilters={setSearchFilter} />
+					)}
+
+					<div className="my-2">
+						<Pagination firstPage={1} lastPage={10} currPage={5} />
+					</div>
 					<div className="my-2">
 						<DropDownMenu
 							label="sort by:"
 							menus={sortByMenus}
 							selectedIndx={selectedSortIndx}
 							setSelectedIndx={setSelectedSortIndx}
-							menuClassName="w-[18rem]"
+							menuClassName="w-[18rem] text-sm"
+							menuItemClassName=" text-sm"
 						/>
 					</div>
 				</div>
+
 				<PublicationLists lists={props.researchDetail.publications} />
 
-				<Pagination firstPage={1} lastPage={10} currPage={10} />
+				<div className="my-2">
+					<Pagination firstPage={1} lastPage={10} currPage={5} />
+				</div>
 			</div>
 		</div>
 	);
